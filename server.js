57d37d7fb2bfde1e45e2929e6b2d8ef8aaa74af6
hjb <hjb@hjbdeMacBook-Pro-2.local>
2026-06-12 17:56:16 +0800

    fix: use process.env.PORT for cloud deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.static(path.join(__dirname)));
